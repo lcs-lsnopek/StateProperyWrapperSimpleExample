6ae829886e9520aca6c402f7daa06cec1331cbc0
Logan Snopek <LSnopek@lcs.on.ca>
2021-11-29 11:23:23 -0500

    Initial Commit

---
+++ StateProperyWrapperSimpleExample/StateProperyWrapperSimpleExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateProperyWrapperSimpleExampleApp.swift
+//  StateProperyWrapperSimpleExample
+//
+//  Created by Logan Snopek on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct StateProperyWrapperSimpleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
